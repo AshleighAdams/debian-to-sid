cabcb556bbc682d917216a007331912c37ad6e7d
Kate Adams <self@kateadams.eu>
2014-04-19 20:17:56 +0100

    Fixed gnome icon size patch

--- 6.gnome-icon-size-fix.sh
+++ 6.gnome-icon-size-fix.sh
@@ -8,10 +8,10 @@ if [ `cat $status` == "0" ]; then
 	cp /usr/share/gnome-shell/js/ui/messageTray.js /tmp/messageTray.js
 	cp /usr/share/gnome-shell/theme/gnome-shell.css /tmp/gnome-shell.css
 	sudo su -c 'cat /tmp/messageTray.js | sed "s/SOURCE_ICON_SIZE: 48,/SOURCE_ICON_SIZE: 24,/g" > /usr/share/gnome-shell/js/ui/messageTray.js'
-	sudo su -c 'cat /tmp/gnome-shell.css | tr "background-repeat: repeat;
-    height: 72px;" "background-position: 0 0;
-    background-repeat: repeat;
-    height: 48px;" > /usr/share/gnome-shell/theme/gnome-shell.cs'
+	sudo su -c 'cat /tmp/gnome-shell.css | sed "s/background\\-repeat: repeat;\
+    height: 72px;/background-position: 0 0;\
+    background\\-repeat: repeat;\
+    height: 48px;/g" > /usr/share/gnome-shell/theme/gnome-shell.cs'
 	
 fi
 
