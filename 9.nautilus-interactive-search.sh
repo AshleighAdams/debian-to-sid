1c9d0e8faa30a3661d0a420f00f89cc8ad0bced2
Kate Adams <self@kateadams.eu>
2014-08-02 05:15:51 +0100

    Nautalus interactive search!

---
+++ 9.nautilus-interactive-search.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+sudo apt-get build-dep nautilus
+
+sudo rm -rf /tmp/naut # make sure it's gone!
+
+
+mkdir /tmp/naut
+cp  9.interactive_search.patch /tmp/naut/interactive_search.patch
+
+cd /tmp/naut/
+apt-get source nautilus
+
+echo "preparing patch..."
+
+mv `echo */` nautilus-current/
+
+
+echo "applying patch..."
+patch -p0 < ./interactive_search.patch
+
+echo "building..."
+cd nautilus-current/
+./configure prefix=/usr/
+make
+sudo make install
+
+echo "Enabling interactive search"
+gsettings set org.gnome.nautilus.preferences enable-interactive-search true
+
