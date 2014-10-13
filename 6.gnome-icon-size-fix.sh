79b20019292b757bb135380e07d859af1448420d
Kate Adams <self@kateadams.eu>
2014-10-13 15:24:24 +0100

    Fixes to gnome message tray size (allthough still not 100% complete, can't get it to use the JS files)

--- 6.gnome-icon-size-fix.sh
+++ 6.gnome-icon-size-fix.sh
@@ -1,20 +1,42 @@
+#!/bin/bash
+
+if [ "$(id -u)" != "0" ]; then
+	sudo $0 $@
+	exit
+fi
+
 status="./status/6"
 if [ ! -f $status ]; then
 	echo "0" > $status
 fi
 
 if [ `cat $status` == "0" ]; then
+	echo "extracting gnome's JS files"
+	
+	gs=/usr/lib/gnome-shell/libgnome-shell.so
+	
+	mkdir -p /usr/share/gnome-shell/js
+	cd /usr/share/gnome-shell/js
+	
+	mkdir -p ui ui/components ui/status misc perf extensionPrefs gdm portalHelper
+
+	for r in `gresource list $gs`; do
+	  gresource extract $gs $r > ${r/#\/org\/gnome\/shell/.}
+	done
+	cd -
+fi
+
+if [ `cat $status` == "0" ]; then
 	echo "fixing gnome icon size"
 	cp /usr/share/gnome-shell/js/ui/messageTray.js /tmp/messageTray.js
 	cp /usr/share/gnome-shell/theme/gnome-shell.css /tmp/gnome-shell.css
-	sudo su -c 'cat /tmp/messageTray.js | sed "s/SOURCE_ICON_SIZE: 48,/SOURCE_ICON_SIZE: 24,/g" > /usr/share/gnome-shell/js/ui/messageTray.js'
-	sudo su -c 'cat /tmp/gnome-shell.css | sed "s/background\\-repeat: repeat;\
-    height: 72px;/background-position: 0 0;\
+	cat /tmp/messageTray.js | sed "s/SOURCE_ICON_SIZE: 48,/SOURCE_ICON_SIZE: 24,/g" > /usr/share/gnome-shell/js/ui/messageTray.js
+	cat /tmp/gnome-shell.css | sed "s|height: 72px;|background-position: 0 0;\
     background\\-repeat: repeat;\
-    height: 48px;/g" > /usr/share/gnome-shell/theme/gnome-shell.cs'
-	
+    height: 48px;|g" > /usr/share/gnome-shell/theme/gnome-shell.css
 fi
 
-if [ `cat $status` == "1" ]; then
+if [ `cat $status` == "0" ]; then
+	echo "place 'export GNOME_SHELL_JS=/usr/share/gnome-shell/js' in your ~/.gnomerc"
 	echo "done"
 fi
