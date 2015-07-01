25b0bf1b7be7d103fd7eeeb9dbfd6975d57da4aa
Kate Adams <self@kateadams.eu>
2015-07-01 22:24:19 +0100

    Update apt mirrors to use apt-p2p.

--- 1.upgradetosid.sh
+++ 1.upgradetosid.sh
@@ -3,41 +3,33 @@ if [ ! -f ./status/1 ]; then
 fi
 
 if [ `cat ./status/1` == "0" ]; then
-	mirror="http://httpredir.debian.org/debian"
+	sudo apt install apt-p2p && echo "0.5" > ./status/1
+fi
 
-	# if cancel pushed, exit
-	[ $? == "1" ] && exit 1
-	
+if [ `cat ./status/1` == "0.5" ]; then
 	sudo cp 1.source.list.template /etc/apt/sources.list
-	sudo sed "s|MIRROR|$mirror|g" --in-place=".bak" /etc/apt/sources.list
-	
 	echo "1" > ./status/1
 fi
 
 if [ `cat ./status/1` == "1" ]; then
 	echo "updating packages..."
-	sudo apt-get update
+	sudo apt update
 	echo "2" > ./status/1
 fi
 
 if [ `cat ./status/1` == "2" ]; then
 	echo "performing dist-upgrade"
 	echo "2.5" > ./status/1
-	sudo apt-get dist-upgrade -y && echo "3" > ./status/1
+	sudo apt dist-upgrade -y && echo "3" > ./status/1
 fi
 
 if [ `cat ./status/1` == "2.5" ]; then
 	echo "dist-upgrade halted, attempting to resume!"
-	sudo apt-get -f install -y && echo "3" > ./status/1
+	sudo apt -f install -y && echo "3" > ./status/1
 fi
 
 if [ `cat ./status/1` == "3" ]; then
 	echo "4" > ./status/1
-	echo "done; rebooting..."
-	sudo reboot
-	exit
+	echo "done; reboot when ready..."
 fi
 
-if [ `cat ./status/1` == "4" ]; then
-	echo "done!"
-fi
